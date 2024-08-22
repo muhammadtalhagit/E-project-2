43044d1e4bb8ecdbd8b5eaa965f23a3a5b431836
Hassan-Ali021 <hassanaliouk761@gmail.com>
2024-08-22 10:53:33 +0500

    Register Student form work in process

---
+++ E-project/symphonylimited/Controllers/AdminController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace symphonylimited.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            //if(HttpContext.Session.GetString("role") == "admin")
+            //{
+            //    ViewBag.email = HttpContext.Session.GetString("adminEmail");
+            //    return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+            return View();
+        }
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+        
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Login(string email, string pass)
+        {
+            //if (email == "admin@gmail.com" && pass == "123")
+            //{
+            //    HttpContext.Session.SetString("adminEmail", email);
+            //    HttpContext.Session.SetString("role", "admin");
+            //    return RedirectToAction("Index");
+            //}
+            //else if (email == "user@gmail.com" && pass == "123")
+            //{
+            //    HttpContext.Session.SetString("userEmail", email);
+            //    HttpContext.Session.SetString("role", "user");
+            //    return RedirectToAction("Index", "Home");
+            //}
+            //else
+            //{
+            //    ViewBag.msg = "Invalid Credentials";
+            //    return View();
+            //}
+            return View();
+        }
+
+      
+
+        public IActionResult Aboutus()
+        {
+            //if (HttpContext.Session.GetString("role") == "admin")
+            //{
+            //    ViewBag.email = HttpContext.Session.GetString("adminEmail");
+            //    return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+            return View();
+        }
+
+
+        public IActionResult Contactus()
+        {
+            //if (HttpContext.Session.GetString("role") == "admin")
+            //{
+            //    ViewBag.email = HttpContext.Session.GetString("adminEmail");
+            //    return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+            return View();
+        }
+
+
+        public IActionResult academicdepartment()
+        {
+            //if (HttpContext.Session.GetString("role") == "admin")
+            //{
+            //    ViewBag.email = HttpContext.Session.GetString("adminEmail");
+            //    return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+            return View();
+        }
+
+
+        public IActionResult Accountdepartment()
+        {
+            //if (HttpContext.Session.GetString("role") == "admin")
+            //{
+            //    ViewBag.email = HttpContext.Session.GetString("adminEmail");
+            //    return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Login");
+            //}
+            return View();
+        }
+
+
+        public IActionResult LogoutAdmin()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.Session.Remove("role");
+            HttpContext.Session.Remove("adminEmail");
+
+            return RedirectToAction("Login");
+
+        }
+
+
+    }
+}
